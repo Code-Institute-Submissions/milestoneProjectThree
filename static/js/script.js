c3b3177245474757671b0c9651b8b76e199d3577
kencormican <ken.cormican@bt.com>
2020-11-17 21:22:52 +0000

    base.html boilerplate added

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".sidenav").sidenav();
+});
+
+// $('.button-collapse').sidenav({
+//       menuWidth: 300, // Default is 240
+//       edge: 'left', // Choose the horizontal origin
+//       closeOnClick: true // Closes side-nav on <a> clicks
+//     }
+//   );
