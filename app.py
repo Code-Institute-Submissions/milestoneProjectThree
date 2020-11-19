d96c5955daa3d4f3b044ebefe272e29e8ee5f2a1
kencormican <ken.cormican@bt.com>
2020-11-19 11:36:26 +0000

    Register functionality added

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-from werkzeug.security import generate_password_hash, check_password_hash, safe_str_cmp
+from werkzeug.security import generate_password_hash, check_password_hash
 
 if os.path.exists("env.py"):
     import env
@@ -36,8 +36,35 @@ def get_titles():
 # Register View
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
-    return render_template("sign_up.html")
+    if request.method == "POST":
 
+        # store form inputs in variables
+        username = request.form.get("username").lower()
+        password = request.form.get("password")
+        confirmpassword = request.form.get("confirm-password")
+        
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": username.lower()})
+
+        if existing_user:
+            flash('Username already exists. Please Try again?')
+            return redirect(url_for("sign_up"))
+
+        # confirm password
+        if password != confirmpassword:
+            flash("Passwords do not match, please re-enter")
+            return redirect(url_for("sign_up"))
+        
+        # register user to mongodb
+        register = {
+                "username": username,
+                "password": generate_password_hash(password)
+            }
+        mongo.db.users.insert_one(register)
+        flash("Congratulations! You have successfully registered for this service.")
+
+    return render_template("sign_up.html")
 
 
 if __name__ == "__main__":
